12b691bc80f0c4bd81f40383c9fdc340b1dd03e6
devprove-openedx <openedx@devprove.com>
2025-10-06 13:03:12 +0300

    Update webpack.prod.config.js

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,6 +2,6 @@ const { createConfig } = require('@openedx/frontend-build');
 
 const config = createConfig('webpack-prod');
 
-config.module.rules[0].exclude = /node_modules\/(?!(fastest-levenshtein|@edx|@openedx))/;
+config.module.rules[0].exclude = /node_modules\/(?!(fastest-levenshtein|@edx))/;
 
 module.exports = config;
